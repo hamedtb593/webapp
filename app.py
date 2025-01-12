20b26a9af890491b6a77e28f4f6eb086bfd45281
hamedtb593 <hamedtaeb@yahoo.com>
2025-01-11 19:31:24 -0800

    initial commit

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
 import streamlit as st
 st.header('Tossing a Coin')
 
-St.write('It is not a functional application yet. Under construction.')
+st.write('It is not a functional application yet. Under construction.')
